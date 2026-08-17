76c2e48f4e91a85ae643240db296349c622a84ad
gescariz <131615577+gescariz@users.noreply.github.com>
2026-08-17 02:31:34 -0300

    Add files via upload

---
+++ exploracion_estructuras.py
@@ -0,0 +1,15 @@
+peliculas = ["Gladiador", "Deadpool", "La guerra de los mundos"]
+coordenadas = (-34.6625, -58.3678)
+lenguajes_de_programacion = {"python", "C#", "Java", "python"}
+libro = {"nombre":"Yo, Robot",
+         "auto":"Isaac Asimov",
+         "año":"1950"}
+
+print(type(peliculas))
+print(type(coordenadas))
+print(type(lenguajes_de_programacion))
+print(type(libro))
+
+print(peliculas[0])
+#coordenadas[0] = 35
+print(lenguajes_de_programacion)
